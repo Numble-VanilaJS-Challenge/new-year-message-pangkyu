af76609000cd230f5e22fa791f9059c5706db262
pangkyu <baesense@naver.com>
2023-01-13 23:24:53 +0900

    style : upload 페이지 UI 구성

--- project/frontend/static/js/views/Upload.js
+++ project/frontend/static/js/views/Upload.js
@@ -6,6 +6,30 @@ export default class extends AbstractView {
     this.setTitle("upload");
   }
   async getHtml() {
-    return `<p>upload</p>`;
+    // fetch("http://43.201.103.199/post", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     title: "글 제목",
+    //     content: "글 내용",
+    //     image: `https://img.freepik.com/premium-photo/small-tricolor-kitten-meows-floorroom_
+    //     457211-10960.jpg?w=1060`,
+    //   }),
+    // }).then((response) => console.log(response));
+    return `
+    <div class="waves-effect waves-light btn z-depth-3 upload-btn">
+        랜덤 이미지 추가하기 
+    </div>
+    <div>
+      <input type = 'text' placeholder = '글 제목을 작성해주세요.' autoFocus required />
+    </div>
+    <div>
+      <input type = 'text' placeholder = '글 내용을 작성해주세요.' required />
+    </div>
+    <div class="waves-effect waves-light btn z-depth-3 write-btn">
+    글 작성하기
+    </div>`;
   }
 }
