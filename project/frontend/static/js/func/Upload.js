d574a8af7e857cc26469bd0773862d48b7b1257d
pangkyu <baesense@naver.com>
2023-01-14 20:44:27 +0900

    Feat : upload 클릭 핸들러 설정

--- project/frontend/static/js/func/Upload.js
+++ project/frontend/static/js/func/Upload.js
@@ -1,35 +1,41 @@
-// window.onload = function Upload() {
-async function upload() {
-  alert("hi");
-  const uploadButton = document.querySelector(".write-btn");
-  uploadButton.addEventListener("click", click);
+window.onload = async function upload() {
+  const postUploadButton = document.querySelector(".postUpload-btn");
+  const ImageUploadButton = document.querySelector(".imageUpload-btn");
+  // let uploadTitle = document.forms["uploading"]["uploadTitle"].value;
+  // let uploadContent = document.forms["uploading"]["uploadContent"].value;
+  // let uploadImage = document.forms["uploading"]["uploadImage"].value;
+  const titleUpload = document.querySelector(".titleUpload");
 
-  function click(e) {
-    e.preventDefault();
-    window.alert("click");
+  postUploadButton.addEventListener("click", postUploadBtnClick);
+  ImageUploadButton.addEventListener("click", ImageUploadBtnClick);
 
-    const postTitle = document.forms["posting"]["postTitle"].value;
-    const postContent = document.forms["posting"]["postContent"].value;
-    const postImage = document.forms["posting"]["postImage"].value;
-
-    if (postTitle === "" || postContent === "" || postImage === "") {
-      alert("비어있는 항목이 있습니다. 입력후 다시 등록해주세요");
-      return false;
-    }
-    fetch("http://43.201.103.199/post", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title: postTitle,
-        content: postContent,
-        image: `https://img.freepik.com/premium-photo/small-tricolor-kitten-meowsfloor-
-        room_457211-10960.jpg?w=1060`,
-        // image: postImage,
-      }),
-    }).then((response) => console.log(response));
+  function ImageUploadBtnClick(e) {
+    window.alert("이미지");
   }
-}
+
+  function postUploadBtnClick(e) {
+    window.alert("글 등록");
+    console.log(titleUpload.value);
+    // console.log(uploadContent);
+
+    // if (postTitle === "" || postContent === "" || postImage === "") {
+    //   alert("비어있는 항목이 있습니다. 입력후 다시 등록해주세요");
+    //   return false;
+    // }
+    // fetch("http://43.201.103.199/post", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     title: postTitle,
+    //     content: postContent,
+    //     image: `https://img.freepik.com/premium-photo/small-tricolor-kitten-meowsfloor-
+    //     room_457211-10960.jpg?w=1060`,
+    //     // image: postImage,
+    //   }),
+    // }).then((response) => console.log(response));
+  }
+};
 
 // async function randomImage() {}
--- project/frontend/static/js/views/Upload.js
+++ project/frontend/static/js/views/Upload.js
@@ -3,24 +3,23 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
   constructor() {
     super();
-    this.setTitle("upload");
+    this.setTitle("신년 인사말을 등록해주세요");
   }
+
   async getHtml() {
     return `
-    <form method = 'post' name = 'posting'>
-    <div class="waves-effect waves-light btn z-depth-3 upload-btn" name = 'postImage'>
+    <div class="waves-effect waves-light btn z-depth-3 imageUpload-btn" name = 'uploadImage' class = 'ImageUploadBtn' value = '' >
         랜덤 이미지 추가하기 
     </div>
     <div>
-      <input type = 'text' name = 'postTitle' placeholder = '글 제목을 작성해주세요.' autoFocus required />
+      <input type = 'text'  name = 'uploadTitle' placeholder = '글 제목을 작성해주세요.' class = 'titleUpload' autoFocus required />
     </div>
     <div>
-      <input type = 'text' name = 'postContent' placeholder = '글 내용을 작성해주세요.' required />
+      <input type = 'text' name = 'uploadContent' placeholder = '글 내용을 작성해주세요.'  class = 'contentUpload' required />
     </div>
-    <div class="waves-effect waves-light btn z-depth-3 write-btn" >
+    <div class="waves-effect waves-light btn z-depth-3 postUpload-btn" >
     글 작성하기
     </div>
-    </form>
     `;
   }
 }
