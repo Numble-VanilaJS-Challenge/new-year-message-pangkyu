085ad42661050ac49588f960c1da74b141740923
pangkyu <baesense@naver.com>
2023-01-18 12:50:36 +0900

    Style : post 페이지 UI 구성

--- project/frontend/static/js/views/Post.js
+++ project/frontend/static/js/views/Post.js
@@ -21,34 +21,42 @@ export default class extends AbstractView {
     const getAPI = await this.getAPI();
     const postDay = getAPI.post.createdAt.substr(0, 10);
     console.log(getAPI.post.createdAt);
+    console.log(getAPI.comments);
 
     return /* html */ `
-        <div>
+        <div class = 'postSection'>
             <div class = 'post__header'>
-                <div class = 'post__header-title'><p>${getAPI.post.title}</p></div>
-                <div><p>${postDay}</p></div>
+                <div class = 'post__header-title'><p>${
+                  getAPI.post.title
+                }</p></div>
+                <div class = 'post__header-day'><p>${postDay}</p></div>
             </div>
-            <div>
-                <img src = ${getAPI.post.image} class = 'post-image' />
+            <img src = ${getAPI.post.image} class = 'post-image' />
+            <div class = 'post__content'><p>${getAPI.post.content}</p></div>
+            <div class = 'post__button'>
+                <div class="waves-effect waves-light btn post__button-edit">수정</div>
+                <div class="waves-effect waves-light btn post__button-delete">삭제</div>
             </div>
-            <div>
-                <div><p>${getAPI.post.content}</p></div>
-                <div>
-                <div class="waves-effect waves-light btn">수정</div>
-                <div class="waves-effect waves-light btn">삭제</div>
+       
+        </div>
+        <p>댓글 리스트</p>
+   
+        ${getAPI.comments
+          .map(
+            (item) =>
+              ` 
+                <div class = 'post__commentlist-area'>
+                  <div class = 'post__commentlist-content'>${item.content} </div>
+                  <div class="waves-effect waves-light btn post__commentlist-button">삭제</div>
                 </div>
-            </div>
-        <div>
-        <div>
-            <p>댓글 리스트</p>
+              `
+          )
+          .join("")}
+        <div class = 'post__comment'> 
+            <input type = 'text' placeholder = '댓글을 입력할 수 있습니다.' class = 'post__comment-box' style = 'width : 85%'/>
+            <div class="waves-effect waves-light btn post__comment-btn" style = 'width : 15%'>입력</div>
         </div>
-        <div>
-            <div> 
-                <input type = 'text' placeholder = '댓글을 입력할 수 있습니다.'/>
-                <div class="waves-effect waves-light btn">입력</div>
-            </div>
-            <p>댓글</p>
-        </div>
+        
     `;
   }
 }
