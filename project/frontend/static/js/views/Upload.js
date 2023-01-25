9371405fa325f97568c95232023325e39de3f502
pangkyu <baesense@naver.com>
2023-01-25 22:58:33 +0900

    refactor : 주석제거

--- project/frontend/static/js/views/Upload.js
+++ project/frontend/static/js/views/Upload.js
@@ -6,17 +6,30 @@ export default class extends AbstractView {
     this.setTitle("HAPPY 2023 - 신년 인사를 작성해봐요!");
   }
 
+  // async getImage() {
+  //   const ImageUploadButton = document.querySelector(".imageUpload-btn");
+  //   let ImageUpload = "";
+  //   await ImageUploadButton.addEventListener("click", ImageUploadBtnClick);
+
+  //   await fetch(`https://source.unsplash.com/random`).then(
+  //     (response) => (ImageUpload = response.url)
+  //   );
+  //   return ImageUpload;
+  // }
+
   async getHtml() {
-    const uploadScript = document.createElement("script");
+    // const image = await this.getImage();
+    // alert(image);
+    const uploadScript = await document.createElement("script");
     uploadScript.setAttribute("type", "module");
     uploadScript.setAttribute("src", "./static/js/func/Upload.js");
     uploadScript.setAttribute("crossorigin", "use-credentials");
-    uploadScript.setAttribute("async", "");
+    uploadScript.setAttribute("defer", "");
 
     document.getElementById("root").appendChild(uploadScript);
 
     return /* html*/ `
-    <div class="waves-effect waves-light btn z-depth-3 imageUpload-btn" class = 'ImageUploadBtn'  >
+    <div class="waves-effect waves-light btn z-depth-3 imageUpload-btn ImageUploadBtn"  >
         랜덤 이미지 추가하기 
     </div>
     <div>
